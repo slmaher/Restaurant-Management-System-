93bc047c045dd7424a6afbc94e5e01f05e382aa4
Salma Maher <salmamaher867@gmail.com>
2025-12-19 02:38:01 +0200

    Implement Singleton pattern for RestaurantSys

---
+++ src/system/RestaurantSystem.java
@@ -0,0 +1,29 @@
+package system;
+
+public class RestaurantSystem {
+    
+  RestaurantSystem instance;
+  
+    private RestaurantSystem() {
+        System.out.println("RestaurantSystem instance created.");
+    }
+   
+    public static RestaurantSystem getInstance() {
+        if (instance == null) {
+            instance = new RestaurantSystem();
+        }
+        return instance;
+    }
+    
+
+    public void startSystem() {
+        System.out.println("Restaurant Management System is starting...");
+        System.out.println("System initialized successfully!");
+    }
+    
+    public void showStatus() {
+        System.out.println("Restaurant Management System is running.");
+        System.out.println("Status: Active");
+    }
+}
+
